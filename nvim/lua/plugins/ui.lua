4d974e6a1cfb73ccdc05884fdc347c447d669c6f
ino <87593133+inoUwU@users.noreply.github.com>
2024-02-09 22:12:17 +0900

    configファイルをpluginファイルへ統合

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -14,7 +14,7 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("config/lualine")
+      require("lualine").setup()
     end,
   },
   -- ファイルエクスプローラー
@@ -28,9 +28,10 @@ return {
         "<C-n>",
         ":NvimTreeFocus<CR>",
         mode = "n",
+        silent = true,
         desc = "ファイルエクスプローラーにフォーカスする",
       },
-      { "<leader>e", ":NvimTreeToggle<CR>", desc = "ファイルエクスプローラーを開く" },
+      { "<leader>e", ":NvimTreeToggle<CR>", silent = true, desc = "ファイルエクスプローラーを開く" },
     },
     cnd = "NvimTreeOpen",
     config = function()
@@ -185,6 +186,22 @@ return {
     config = function()
       require("notify").setup({
         background_colour = "#000000",
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
       })
     end,
   },
