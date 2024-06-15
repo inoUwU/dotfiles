7ad00c299560a33f173c87b0fa3834d788a302b9
ino <87593133+inoUwU@users.noreply.github.com>
2024-06-16 02:25:09 +0900

    hop.nvimを導入

---
+++ nvim/lua/plugins/utils/hop-nvim.lua
@@ -0,0 +1,12 @@
+return {
+  "phaazon/hop.nvim",
+  branch = "v2",
+  config = function()
+    require("hop").setup({
+      multi_windows = true,
+    })
+  end,
+  keys = {
+    { mode = "n", "<leader>s", "<cmd>HopChar1<CR>", desc = "" },
+  },
+}
