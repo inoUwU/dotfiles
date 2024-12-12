1d39a748c9daaa002c0b2b5c86d18a8066030296
ino <87593133+inoUwU@users.noreply.github.com>
2024-12-12 23:54:47 +0900

    GitHub-copilotの設定

--- nvim/lua/plugins/ai/github-copilot.lua
+++ nvim/lua/plugins/ai/github-copilot.lua
@@ -1,4 +1,11 @@
 return {
-  "github/copilot.vim",
-  lazy = false,
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  config = function()
+    require("copilot").setup({
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      copilot_node_command = "node",
+    })
+  end,
 }
--- nvim/lua/plugins/lsp/nvim-cmp.lua
+++ nvim/lua/plugins/lsp/nvim-cmp.lua
@@ -16,9 +16,17 @@ return {
 
     local lspkind = require("lspkind")
 
+    -- TODO: Fix deprecated
+    local has_words_before = function()
+      if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+        return false
+      end
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+    end
+
     -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
     require("luasnip.loaders.from_vscode").lazy_load()
-
     cmp.setup({
       completion = {
         completeopt = "menu,menuone,preview,noselect",
@@ -36,20 +44,30 @@ return {
         ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
         ["<C-e>"] = cmp.mapping.abort(), -- close completion window
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<Tab>"] = vim.schedule_wrap(function(fallback)
+          if cmp.visible() and has_words_before() then
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+          else
+            fallback()
+          end
+        end),
       }),
 
       -- sources for autocompletion
       sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "luasnip" }, -- snippets
-        { name = "buffer" }, -- text within current buffer
-        { name = "path" }, -- file system paths
+        { name = "copilot" },
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "luasnip", group_index = 2 }, -- snippets
+        { name = "buffer", group_index = 2 }, -- text within current buffer
+        { name = "path", group_index = 2 }, -- file system paths
       }),
 
       -- configure lspkind for vs-code like pictograms in completion menu
       formatting = {
         format = lspkind.cmp_format({
+          mode = "symbol",
           maxwidth = 50,
+          symbol_map = { Copilot = "" },
           ellipsis_char = "...",
         }),
       },
