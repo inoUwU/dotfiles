7ac414f07aa2237bee6bdc23a31af9d84130a167
inoUwU <inokawa.kouta@gmail.com>
2025-03-21 22:58:44 +0900

    replace indent plugins

--- nvim/lua/plugins/utils/snacks.lua
+++ nvim/lua/plugins/utils/snacks.lua
@@ -1,6 +1,10 @@
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    indent = { enabled = true },
+    animate = { enabled = true },
     bigfile = { enabled = true },
     notifier = { enabled = true },
     quickfile = { enabled = true },
