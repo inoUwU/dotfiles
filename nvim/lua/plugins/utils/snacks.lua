b18667dc3b7bd57dde802bf857730e7bba30b372
inoUwU <inokawa.kouta@gmail.com>
2025-03-23 23:47:57 +0900

    format snacks.lua

--- nvim/lua/plugins/utils/snacks.lua
+++ nvim/lua/plugins/utils/snacks.lua
@@ -27,11 +27,6 @@ return {
       return visual and visual.text or ""
     end
 
-    -- keym󰵞ap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-    -- keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-    -- keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    -- keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-    --
     keymap.set("v", "<leader>ff", function()
       picker.grep({
         on_show = function()
@@ -39,7 +34,6 @@ return {
         end,
       })
     end, { desc = "Fuzzy find files in cwd" })
-
     keymap.set("n", "<leader>ff", function()
       picker.files()
     end, { desc = "Fuzzy find files in cwd" })
@@ -52,14 +46,6 @@ return {
     keymap.set("n", "<leader>fc", function()
       picker.colorschemes()
     end, { desc = "Find colortheme" })
-
-    -- keymap.set("n","<leader>fa", function()
-    -- 	picker.pickers()
-    -- end,{ desc = "Show all pickers "}
-
-    -- keymap.set("n", "<leader>ft", function()
-    --   picker.todo_comments()
-    -- end, { desc = "Find todos" })
     keymap.set("n", "<leader>wz", function()
       zen.zen()
     end, { desc = "Toggle zen mode" })
