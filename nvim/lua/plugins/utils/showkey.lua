801ac61cd116e53f45826529f9eaf53c40269f1d
ino <87593133+inoUwU@users.noreply.github.com>
2025-03-25 23:25:20 +0900

    add showkey

---
+++ nvim/lua/plugins/utils/showkey.lua
@@ -0,0 +1,23 @@
+return {
+  "nvzone/showkeys",
+  dependencies = { "folke/snacks.nvim" },
+  opts = {
+    timeout = 3,
+    maxkeys = 5,
+  },
+  init = function()
+    local showkeys = require("showkeys")
+    local snacks = require("snacks")
+    snacks
+      .toggle({
+        name = "Showkeys",
+        get = function()
+          return require("showkeys.state").visible
+        end,
+        set = function()
+          showkeys.toggle()
+        end,
+      })
+      :map("<leader>uk")
+  end,
+}
