a86465ea042293f31b3b5b9766a1e9c0cb7ec6a4
ino <87593133+inoUwU@users.noreply.github.com>
2025-04-09 00:07:44 +0900

    remap lspsaga

--- nvim/lua/plugins/lsp/lspsaga.lua
+++ nvim/lua/plugins/lsp/lspsaga.lua
@@ -1,19 +1,23 @@
 return {
   "nvimdev/lspsaga.nvim",
   dependencies = {
-    "nvim-treesitter/nvim-treesitter", -- optional
-    "nvim-tree/nvim-web-devicons", -- optional
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("lspsaga").setup({
-      show_file = false,
-    })
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+    require("lspsaga").setup({})
+    local keymap = vim.keymap
     keymap.set("n", "<leader>lo", "<cmd>Lspsaga outline<cr>", { desc = "OutLine" })
     keymap.set("n", "<leader>lf", "<cmd>Lspsaga finder<cr>", { desc = "Finder" })
     keymap.set("n", "<leader>lp", "<cmd>Lspsaga peek_definition<cr>", { desc = "Peek definition" })
     keymap.set("n", "<leader>la", "<cmd>Lspsaga code_action<cr>", { desc = "Code action" })
+    keymap.set("n", "<leader>lsb", "<cmd>Lspsaga show_buf_diagnostics<cr>", { desc = "Show buf diagnostics" })
+    keymap.set(
+      "n",
+      "<leader>lsw",
+      "<cmd>Lspsaga show_workspace_diagnostics ++float<cr>",
+      { desc = "Show workspace diagnostics" }
+    )
     keymap.set("n", "K", "<cmd>Lspsaga hover_doc<cr>", { noremap = true, desc = "Hover doc" })
   end,
 }
