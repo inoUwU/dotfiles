f1d70f335cd880ef044d99c82f8cc7ed52f9f001
inoUwU <inokawa.kouta@gmail.com>
2025-06-08 15:56:37 +0900

    refactor:lspconfig

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -9,100 +9,86 @@ return {
     local lspconfig = require("lspconfig")
     local util = require("lspconfig.util")
     local keymap = vim.keymap -- for conciseness
-    local opts = { noremap = true, silent = true }
-    local on_attach = function(client, bufnr)
-      opts.buffer = bufnr
+    local base_opts = { noremap = true, silent = true }
+
+    local function on_attach(client, bufnr)
+      local opts = vim.tbl_extend("force", base_opts, { buffer = bufnr })
 
       opts.desc = "Go to declaration"
-      keymap.set("n", "<leader>ld", vim.lsp.buf.declaration, opts) -- go to declaration
+      keymap.set("n", "<leader>ld", vim.lsp.buf.declaration, opts)
 
       opts.desc = "Smart rename"
-      keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts) -- smart rename
+      keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts)
 
       opts.desc = "Show line diagnostics"
-      keymap.set("n", "<leader>ll", vim.diagnostic.open_float, opts) -- show diagnostics for line
+      keymap.set("n", "<leader>ll", vim.diagnostic.open_float, opts)
     end
 
+    local servers = {
+      html = {},
+      cssls = {},
+      tailwindcss = {},
+      prismals = {},
+      pyright = {},
+      gleam = {},
+      gopls = {
+        filetypes = { "go", "gomod", "gowork", "gotmpl" },
+      },
+      yamlls = {
+        filetypes = { "yaml", "yml" },
+      },
+      graphql = {
+        filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+      },
+      emmet_ls = {
+        filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+      },
+      gh_actions_ls = {
+        filetypes = { "yaml.github" },
+        root_dir = util.root_pattern(".github"),
+        single_file_support = true,
+      },
+      lua_ls = {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+            workspace = {
+              library = {
+                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                [vim.fn.stdpath("config") .. "/lua"] = true,
+              },
+            },
+          },
+        },
+      },
+      svelte = {
+        on_attach = function(client, bufnr)
+          on_attach(client, bufnr)
+          vim.api.nvim_create_autocmd("BufWritePost", {
+            pattern = { "*.js", "*.ts" },
+            callback = function(ctx)
+              if client.name == "svelte" then
+                client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.file })
+              end
+            end,
+          })
+        end,
+      },
+    }
+
+    -- Set diagnostic signs
     local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
     for type, icon in pairs(signs) do
       local hl = "DiagnosticSign" .. type
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     end
-    lspconfig["html"].setup({
-      on_attach = on_attach,
-    })
-    lspconfig["cssls"].setup({
-      on_attach = on_attach,
-    })
-    lspconfig["tailwindcss"].setup({
-      on_attach = on_attach,
-    })
-    lspconfig["svelte"].setup({
-      on_attach = function(client, bufnr)
-        on_attach(client, bufnr)
 
-        vim.api.nvim_create_autocmd("BufWritePost", {
-          pattern = { "*.js", "*.ts" },
-          callback = function(ctx)
-            if client.name == "svelte" then
-              client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.file })
-            end
-          end,
-        })
-      end,
-    })
-    lspconfig["prismals"].setup({
-      on_attach = on_attach,
-    })
-    lspconfig["graphql"].setup({
-      on_attach = on_attach,
-      filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-    })
-    lspconfig["emmet_ls"].setup({
-      on_attach = on_attach,
-      filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
-    })
-    lspconfig["pyright"].setup({
-      on_attach = on_attach,
-    })
-    lspconfig["gopls"].setup({
-      on_attach = on_attach,
-      filetypes = { "go", "gomod", "gowork", "gotmpl" },
-    })
-    lspconfig["yamlls"].setup({
-      on_attach = on_attach,
-      filetypes = { "yaml", "yml" },
-    })
-
-    lspconfig.gh_actions_ls.setup({
-      on_attach = on_attach,
-      filetypes = { "yaml.github" },
-      root_dir = util.root_pattern(".github"),
-      single_file_support = true,
-    })
-
-    lspconfig.gleam.setup({
-      on_attach = on_attach,
-    })
-
-    -- configure lua server (with special settings)
-    lspconfig["lua_ls"].setup({
-      on_attach = on_attach,
-      settings = { -- custom settings for lua
-        Lua = {
-          -- make the language server recognize "vim" global
-          diagnostics = {
-            globals = { "vim" },
-          },
-          workspace = {
-            -- make language server aware of runtime files
-            library = {
-              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-              [vim.fn.stdpath("config") .. "/lua"] = true,
-            },
-          },
-        },
-      },
-    })
+    -- Setup all language servers
+    for server, config in pairs(servers) do
+      config.on_attach = config.on_attach or on_attach
+      lspconfig[server].setup(config)
+    end
   end,
 }
