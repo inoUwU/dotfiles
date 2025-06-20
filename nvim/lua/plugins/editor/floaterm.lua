c77c324a21750569992f76719d81e977e90de94d
fract <87593133+inoUwU@users.noreply.github.com>
2025-06-20 23:11:26 +0900

    add floaterm

---
+++ nvim/lua/plugins/editor/floaterm.lua
@@ -0,0 +1,9 @@
+return {
+  "nvzone/floaterm",
+  dependencies = "nvzone/volt",
+  opts = {},
+  cmd = "FloatermToggle",
+  keys = {
+    { mode = "n", "<leader>t", "<cmd>FloatermToggle<CR>", desc = "ToggleFloaterm" },
+  },
+}
