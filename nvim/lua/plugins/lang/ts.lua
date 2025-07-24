160f35e5c02f9480554a9e1ca9e60cabeb5cf1cc
fract <87593133+inoUwU@users.noreply.github.com>
2025-07-24 22:11:24 +0900

    update

--- nvim/lua/plugins/lang/ts.lua
+++ nvim/lua/plugins/lang/ts.lua
@@ -2,4 +2,15 @@ return {
   "pmizio/typescript-tools.nvim",
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
   opts = {},
+
+  config = function()
+    require("typescript-tools").setup({
+      settings = {
+        tsserver = {
+          -- tsserver側のdiagnosticsを無効化
+          diagnostics = false,
+        },
+      },
+    })
+  end,
 }
