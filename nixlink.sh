fd3b6213a46066e98852c784ce935a81d8380021
inoUwU <inokawa.kouta@gmail.com>
2025-10-08 22:54:42 +0900

    update nixos

--- nixlink.sh
+++ nixlink.sh
@@ -3,7 +3,6 @@
 set -e
 
 DOTFILES_DIR="$HOME/dotfiles/nixos"
-WAYBAR_DIR="$HOME/dotfiles/waybar"
 
 echo "Linking NixOS system configs..."
 sudo ln -sf "$DOTFILES_DIR/configuration.nix" /etc/nixos/configuration.nix
@@ -14,8 +13,8 @@ mkdir -p ~/.config/home-manager
 ln -sf "$DOTFILES_DIR/home.nix" ~/.config/home-manager/home.nix
 
 
-sudo ln -sf "$WAYBAR_DIR/config" ~/.config/waybar/config
-sudo ln -sf "$WAYBAR_DIR/style.css" ~/.config/waybar/style.css
+sudo ln -sf "$DOTFILES_DIR/waybar/config" ~/.config/waybar/config
+sudo ln -sf "$DOTFILES_DIR/waybar/style.css" ~/.config/waybar/style.css
 
 
 echo "All symlinks updated!"
