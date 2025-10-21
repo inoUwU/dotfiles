d8a96ca17bd3d163da46bb1050780b9e43756a70
fract <87593133+inoUwU@users.noreply.github.com>
2025-10-21 23:55:54 +0900

    update treesitter

--- nvim/init.lua
+++ nvim/init.lua
@@ -50,3 +50,12 @@ vim.api.nvim_create_autocmd("LspAttach", {
     })
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("vim-treesitter-start", {}),
+  callback = function(ctx)
+    -- 必要に応じて`ctx.match`に入っているファイルタイプの値に応じて挙動を制御
+    -- `pcall`でエラーを無視することでパーサーやクエリがあるか気にしなくてすむ
+    pcall(vim.treesitter.start)
+  end,
+})
--- nvim/lua/plugins/editor/treesitter.lua
+++ nvim/lua/plugins/editor/treesitter.lua
@@ -1,34 +1,43 @@
+-- return {
+--   "nvim-treesitter/nvim-treesitter",
+--   branch = "main",
+--   build = ":TSUpdate",
+--   config = function()
+--     local configs = require("nvim-treesitter.configs")
+--
+--     configs.setup({
+--       install = { "zig" },
+--       ensure_installed = {
+--         "c",
+--         "cpp",
+--         "tsx",
+--         "lua",
+--         "vim",
+--         "vimdoc",
+--         "query",
+--         "rust",
+--         "go",
+--         "ruby",
+--         "javascript",
+--         "html",
+--         "typescript",
+--         "markdown",
+--         "markdown_inline",
+--         "yaml",
+--         "gleam",
+--         "elm",
+--       },
+--       sync_install = false,
+--       highlight = { enable = true },
+--       indent = { enable = true },
+--     })
+--   end,
+-- }
+
 return {
   "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
+  branch = "main",
   config = function()
-    local configs = require("nvim-treesitter.configs")
-
-    configs.setup({
-      install = { "zig" },
-      ensure_installed = {
-        "c",
-        "cpp",
-        "tsx",
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-        "rust",
-        "go",
-        "ruby",
-        "javascript",
-        "html",
-        "typescript",
-        "markdown",
-        "markdown_inline",
-        "yaml",
-        "gleam",
-        "elm",
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
+    require("nvim-treesitter").setup({})
   end,
 }
