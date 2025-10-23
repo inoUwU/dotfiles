3e6925802772e0ad7ebbbeb9a4ce787923fe9763
fract <87593133+inoUwU@users.noreply.github.com>
2025-10-24 00:52:49 +0900

    cleanup treesitter

--- nvim/lua/plugins/editor/treesitter.lua
+++ nvim/lua/plugins/editor/treesitter.lua
@@ -1,39 +1,3 @@
--- return {
---   "nvim-treesitter/nvim-treesitter",
---   branch = "main",
---   build = ":TSUpdate",
---   config = function()
---     local configs = require("nvim-treesitter.configs")
---
---     configs.setup({
---       install = { "zig" },
---       ensure_installed = {
---         "c",
---         "cpp",
---         "tsx",
---         "lua",
---         "vim",
---         "vimdoc",
---         "query",
---         "rust",
---         "go",
---         "ruby",
---         "javascript",
---         "html",
---         "typescript",
---         "markdown",
---         "markdown_inline",
---         "yaml",
---         "gleam",
---         "elm",
---       },
---       sync_install = false,
---       highlight = { enable = true },
---       indent = { enable = true },
---     })
---   end,
--- }
-
 return {
   "nvim-treesitter/nvim-treesitter",
   branch = "main",
