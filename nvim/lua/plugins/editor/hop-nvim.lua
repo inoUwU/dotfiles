d3c7e427eae915bbd3f26ca7761112323d497c38
ino <87593133+inoUwU@users.noreply.github.com>
2026-01-31 22:27:20 +0900

    neovim:update keymap

--- nvim/lua/plugins/editor/hop-nvim.lua
+++ nvim/lua/plugins/editor/hop-nvim.lua
@@ -1,14 +1,10 @@
 return {
   "phaazon/hop.nvim",
   branch = "v2",
-  config = function()
-    require("hop").setup({
-      multi_windows = true,
-    })
-  end,
+  opts = {
+    multi_windows = true,
+  },
   keys = {
-    { mode = "n", "<leader>jl", "<cmd>HopLine<CR>", desc = "HopLine" },
-    { mode = "n", "<leader>jj", "<cmd>HopWord<CR>", desc = "HopWord" },
-    { mode = "n", "<leader>ja", "<cmd>HopAnywhere<CR>", desc = "HopAnywhere" },
+    { mode = "n", "<leader>j", "<cmd>HopWord<CR>", desc = "HopWord" },
   },
 }
--- nvim/lua/plugins/editor/todo-comments.lua
+++ nvim/lua/plugins/editor/todo-comments.lua
@@ -1,6 +1,6 @@
 return {
   "folke/todo-comments.nvim",
-  event = { "BufReadPost", "BufNewFile" },
+  lazy = false,
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {},
 }
