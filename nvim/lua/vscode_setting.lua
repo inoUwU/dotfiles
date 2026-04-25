d7fb46547156ec0856b317518db3075aa01c729e
ino <87593133+inoUwU@users.noreply.github.com>
2026-04-25 14:54:05 +0900

    fix:windowsのvscodeで使用される場合もwin32yankを使用するように設定

--- nvim/lua/vscode_setting.lua
+++ nvim/lua/vscode_setting.lua
@@ -3,3 +3,16 @@ vim.opt.clipboard = "unnamedplus"
 -- TODO: Dupulicated
 -- Visual mode keymap to prevent clipboard overwrite on paste
 vim.api.nvim_set_keymap("x", "p", '"_dP', { noremap = true, silent = true })
+if vim.fn.has("win64") == 1 then
+  vim.g.clipboard = {
+    name = "win32yank",
+    copy = {
+      ["+"] = "C:\\MyTools\\win32yank-x64\\win32yank.exe -i --crlf",
+      ["*"] = "C:\\tools\\win32yank-x64\\win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "C:\\MyTools\\win32yank-x64\\win32yank.exe -o --lf",
+      ["*"] = "C:\\MyTools\\win32yank-x64\\win32yank.exe -o --lf",
+    },
+  }
+end
