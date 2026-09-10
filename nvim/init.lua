6a58c927c7e9fdf281272ad7f72c0aa572be6731
FRACT <inokawa.kouta@gmail.com>
2026-09-11 00:08:27 +0900

    nvim:vim-treesitter-startの通知を削除

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,12 +38,3 @@ vim.api.nvim_create_autocmd("FileType", {
     print("GitHub Actions YAML file detected!")
   end,
 })
-
-vim.api.nvim_create_autocmd("FileType", {
-  group = vim.api.nvim_create_augroup("vim-treesitter-start", {}),
-  callback = function(ctx)
-    -- 必要に応じて`ctx.match`に入っているファイルタイプの値に応じて挙動を制御
-    -- `pcall`でエラーを無視することでパーサーやクエリがあるか気にしなくてすむ
-    pcall(vim.treesitter.start)
-  end,
-})
